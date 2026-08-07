3324cefce4db4116c26b8ab2a7c19c7f3321a69c
austin28-maker <1160278698@qq.com>
2026-08-07 22:47:58 +0800

    Solve ListNode_four

---
+++ hot100_solutions/0142_detectCycle/solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                ListNode index1 = slow;
+                ListNode index2 = head;
+                while(index1 != index2){
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index1;
+            }
+        }
+        return null;
+    }
+}
+
+// class ListNode{
+//     int val;
+//     ListNode next;
+//     ListNode(int val){
+//         this.val = val;
+//         next = null;
+//     }
+// }
